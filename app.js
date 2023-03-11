9302d6e7e68733c468b3b68ad8781791c73c4c7d
NPB98 <nishibiswasroy@gmail.com>
2023-03-11 13:05:35 +0530

    NoSQL Task 2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,17 +21,17 @@ app.use((req, res, next) => {
   // User.findById(1)
   //   .then(user => {
   //     req.user = user;
-      next();
+  //     next();
   //   })
   //   .catch(err => console.log(err));
+  next();
 });
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect( ()=> {
-  //console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -1,28 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-let _db;
 
+let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://nishibiswasroy:<passwordmongodb+srv://nishibiswasroy:littlebiswasroy@cluster0.nho40uw.mongodb.net/shop?retryWrites=true&w=majority>@cluster0.ratfyli.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://nishibiswasroy:littlebiswasroy@cluster0.ro9x5ox.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      _db=client.db();
+      _db = client.db();
       callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-const getDb=()=>{
-  if(_db){
+const getDb = () => {
+  if (_db) {
     return _db;
   }
-  throw 'No db found'
-}
+  throw 'No database found!';
+};
 
-exports.mongoConnect=mongoConnect;
-exports.getDb=getDb;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
